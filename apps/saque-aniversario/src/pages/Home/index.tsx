2f734c247fe7f7deba4027a4426d34cd546bc2d5
janilso <jralves@brasilseg.com.br>
2025-01-10 07:41:16 -0300

    feat: adicionado modal sobre

--- apps/saque-aniversario/src/pages/Home/index.tsx
+++ apps/saque-aniversario/src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
-import { BasePage, Button, Input } from '@calculadoras/ui/components';
+import {
+  BasePage,
+  Button,
+  ButtonAbout,
+  Input,
+} from '@calculadoras/ui/components';
 import { IconFgts } from '@calculadoras/ui/icons';
-import { Box, Grid2 } from '@mui/material';
+import { Box, DialogContentText, Grid2, Link } from '@mui/material';
 import React from 'react';
 import { FormProvider } from 'react-hook-form';
 import TabelResult from '../../components/TabelResult';
@@ -65,19 +70,59 @@ const HomePage: React.FC = () => {
               label="Calcular"
             />
           </Grid2>
-
-          {data || loading ? (
-            <TabelResult
-              saldoFgts={data?.saldoFgts}
-              somaLancamentos={data?.somaLancamentos}
-              saldoFuturoTotal={data?.saldoFuturoTotal}
-              previsaoSaque={data?.previsaoSaque}
-              loading={loading}
-              sx={styles.result}
-            />
-          ) : null}
         </Box>
       </FormProvider>
+
+      {data || loading ? (
+        <TabelResult
+          saldoFgts={data?.saldoFgts}
+          somaLancamentos={data?.somaLancamentos}
+          saldoFuturoTotal={data?.saldoFuturoTotal}
+          previsaoSaque={data?.previsaoSaque}
+          loading={loading}
+          sx={styles.result}
+        />
+      ) : null}
+      <ButtonAbout titleModal="Sobre o site e cálculos">
+        <DialogContentText sx={styles.textAbout}>
+          O site Calculadora Saque Aniversário não tem qualquer relação com a
+          instituição bancária CAIXA ou qualquer órgão governamental ou
+          financeiro. O site não armazena qualquer tipo de dado fornecido pelo
+          usuário. Todos os dados solicitados são para uso exclusivo de nossa
+          calculadora do Saque Aniversário.
+        </DialogContentText>
+        <DialogContentText sx={styles.textAbout}>
+          O cálculo do Saque Aniversário é uma previsão não oficial de quanto
+          você poderá sacar no seu mês de aniversário, tendo como base o valor
+          atual de seu saldo do FGTS, seu salário bruto mensal e o seu mês de
+          nascimento. O cálculo é feito da seguinte forma: primeiro, verificamos
+          quanto tempo falta para o seu mês de aniversário. Essa quantidade é
+          multiplicada pelo valor de 8% de seu salário bruto (que é o valor
+          depositado mensalmente pela empresa em que você trabalha na sua conta
+          do FGTS). O resultado desse cálculo inicial é a{' '}
+          <b>Soma de lançamentos</b> futuros de saldo em sua conta do FGTS até
+          sua data de aniversário (levando em consideração que seu salário não
+          será alterado nesse período). Após determinarmos o valor total que
+          você provavelmente terá em sua conta do FGTS no futuro, somamos esse
+          valor ao <b>Saldo do FGTS</b> atual que está em sua conta. O{' '}
+          <b>Somatório total com os lançamentos</b> (incluindo o futuro e atual
+          saldo) é usado para determinar em qual alíquota seu saldo se enquadra,
+          de acordo com a tabela fornecida pela CAIXA
+          <Link
+            target="_blank"
+            href="https://www.caixa.gov.br/beneficios-trabalhador/fgts/saque-FGTS/Paginas/default.aspx"
+          >
+            (você pode consultar clicando aqui)
+          </Link>
+          . Por fim após essa verificação podemos determinar o valor que você
+          sacará em seu mês de aniversário.
+        </DialogContentText>
+        <DialogContentText sx={styles.textAbout}>
+          Vale frisar que esse <b>cálculo não é oficial</b>. É somente uma
+          previsão aproximada de quanto você poderá sacar de saldo em sua conta
+          do FGTS.
+        </DialogContentText>
+      </ButtonAbout>
     </BasePage>
   );
 };
---
+++ packages/ui/components/ButtonAbout/index.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  ButtonBase,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from '@mui/material';
+import React, { PropsWithChildren } from 'react';
+import { IconWarning } from '../../icons';
+import { Button } from '../Button';
+import { styles } from './styles';
+import ButtonAboutTransition from './Transition';
+import { ButtonAboutType } from './types';
+
+const ButtonAbout: React.FC<PropsWithChildren<ButtonAboutType>> = ({
+  textButton = 'Sobre',
+  titleModal,
+  titleModalButton = 'Entendi',
+  children,
+}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Box sx={styles.root} component={ButtonBase} onClick={handleClickOpen}>
+        <IconWarning />
+        <Typography sx={styles.text} variant="body2">
+          {textButton}
+        </Typography>
+      </Box>
+      <Dialog
+        open={open}
+        TransitionComponent={ButtonAboutTransition}
+        keepMounted
+        maxWidth="md"
+        onClose={handleClose}
+      >
+        {titleModal ? (
+          <DialogTitle sx={styles.modalTitle}>{titleModal}</DialogTitle>
+        ) : null}
+        <DialogContent>{children}</DialogContent>
+        <DialogActions>
+          <Button
+            data-gtm-type="click"
+            data-gtm-clicktype="button"
+            data-gtm-name="modal-entendi"
+            onClick={handleClose}
+            label={titleModalButton}
+          />
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default ButtonAbout;
