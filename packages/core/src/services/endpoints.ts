28a36f2f370e8d93cfdd7ceb127ae69693f69338
janilso <jralves@brasilseg.com.br>
2025-01-16 23:22:32 -0300

    new api

--- packages/core/src/services/endpoints.ts
+++ packages/core/src/services/endpoints.ts
@@ -2,9 +2,9 @@ const BASE_URL = (apiUri: string) => `${apiUri || ''}/api`;
 
 const ENDPOINTS = {
   URL_CALC_SAQUE: (apiUri: string) =>
-    `${BASE_URL(apiUri)}/calculadoraSaqueAniversario`,
+    `${BASE_URL(apiUri)}/calculadora/saque-aniversario`,
   URL_SALARIO_LIQUIDO: (apiUri: string) =>
-    `${BASE_URL(apiUri)}/calculadora-salario-liquido`,
+    `${BASE_URL(apiUri)}/calculadora/salario-liquido`,
 };
 
 export { BASE_URL, ENDPOINTS };
